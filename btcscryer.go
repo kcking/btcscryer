a18517443574e7b6c7302577602a7621c24449ae
Kevin King <4kevinking@gmail.com>
2016-12-12 22:22:33 -0500

    use kcking/btcrpcclient fork

--- btcscryer.go
+++ btcscryer.go
@@ -6,34 +6,34 @@ package main
 
 import (
 	"github.com/conformal/btcjson"
-	"github.com/conformal/btcrpcclient"
 	"github.com/conformal/btcutil"
 	"github.com/conformal/btcwire"
+	"github.com/kcking/btcrpcclient"
 	"io/ioutil"
 	"log"
+	"os"
 	"path/filepath"
-    "time"
-    "os"
-    "strconv"
-    "sync"
+	"strconv"
+	"sync"
+	"time"
 )
 
 func mempoolPoll(cli *btcrpcclient.Client) {
-    for {
-        hashes, err := cli.GetRawMempool()
-        if err != nil {
-            log.Fatal(err)
-        }
-        log.Printf("Mempool size: %d", len(hashes))
-        time.Sleep(10 * time.Second)
-    }
+	for {
+		hashes, err := cli.GetRawMempool()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Mempool size: %d", len(hashes))
+		time.Sleep(10 * time.Second)
+	}
 }
 
 func main() {
 
-    // Logging directory
+	// Logging directory
 	btcdHomeDir := btcutil.AppDataDir("btcd", false)
-    dsFileMtx := sync.Mutex{}
+	dsFileMtx := sync.Mutex{}
 	// Only override the handlers for notifications you care about.
 	// Also note most of these handlers will only be called if you register
 	// for notifications.  See the documentation of the btcrpcclient
@@ -45,28 +45,28 @@ func main() {
 		OnBlockDisconnected: func(hash *btcwire.ShaHash, height int32) {
 			log.Printf("Block disconnected: %v (%d)", hash, height)
 		},
-        OnTxAcceptedVerbose: func(txDetails *btcjson.TxRawResult) {
-            log.Printf("Tx Mempooled: %s", txDetails.Txid)
-        },
-        OnTxDoubleSpent: func(mempoolTxHash *btcwire.ShaHash, incomingTxHash *btcwire.ShaHash, isInBlock bool) {
-            dsFileMtx.Lock()
-            defer dsFileMtx.Unlock()
-            log.Println(mempoolTxHash.String() + "," + incomingTxHash.String() + "," + strconv.FormatBool(isInBlock))
+		OnTxAcceptedVerbose: func(txDetails *btcjson.TxRawResult) {
+			log.Printf("Tx Mempooled: %s", txDetails.Txid)
+		},
+		OnTxDoubleSpent: func(mempoolTxHash *btcwire.ShaHash, incomingTxHash *btcwire.ShaHash, isInBlock bool) {
+			dsFileMtx.Lock()
+			defer dsFileMtx.Unlock()
+			log.Println(mempoolTxHash.String() + "," + incomingTxHash.String() + "," + strconv.FormatBool(isInBlock))
 
-            f, err := os.OpenFile(filepath.Join(btcdHomeDir, "doublespends.log"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
-            if err != nil {
-                log.Printf("Error opening doublespend file: %v", err)
-            }
-            defer f.Close()
+			f, err := os.OpenFile(filepath.Join(btcdHomeDir, "doublespends.log"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+			if err != nil {
+				log.Printf("Error opening doublespend file: %v", err)
+			}
+			defer f.Close()
 
-            if _, err = f.WriteString(mempoolTxHash.String() + "," + incomingTxHash.String() + "," + strconv.FormatBool(isInBlock) + "\n"); err != nil {
-                log.Printf("Error writing doublespend file: %v", err)
-            }
-        },
+			if _, err = f.WriteString(mempoolTxHash.String() + "," + incomingTxHash.String() + "," + strconv.FormatBool(isInBlock) + "\n"); err != nil {
+				log.Printf("Error writing doublespend file: %v", err)
+			}
+		},
 	}
 
 	// Connect to local btcd RPC server using websockets.
-    log.Println("Logging double spends to " + btcdHomeDir)
+	log.Println("Logging double spends to " + btcdHomeDir)
 	certs, err := ioutil.ReadFile(filepath.Join(btcdHomeDir, "rpc.cert"))
 	if err != nil {
 		log.Fatal(err)
@@ -89,9 +89,9 @@ func main() {
 	}
 	log.Println("NotifyBlocks: Registration Complete")
 
-    if err := client.NotifyNewTransactions(true); err != nil {
-        log.Fatal(err)
-    }
+	if err := client.NotifyNewTransactions(true); err != nil {
+		log.Fatal(err)
+	}
 	log.Println("NotifyNewTransactions: Registration Complete")
 
 	// Get the current block count.
@@ -101,18 +101,18 @@ func main() {
 	}
 	log.Printf("Block count: %d", blockCount)
 
-    txHash, err := btcwire.NewShaHashFromStr("9e4cca76c166d0c165709ee656056e89fa7a493f7fb7d80e2ba4dbeef098f066")
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Printf("txhash: %v", txHash)
-    rawTx, err := client.GetRawTransactionVerbose(txHash)
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Printf("Found tx with hash %s, contains %d outputs.", txHash, len(rawTx.Vout))
+	txHash, err := btcwire.NewShaHashFromStr("9e4cca76c166d0c165709ee656056e89fa7a493f7fb7d80e2ba4dbeef098f066")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("txhash: %v", txHash)
+	rawTx, err := client.GetRawTransactionVerbose(txHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Found tx with hash %s, contains %d outputs.", txHash, len(rawTx.Vout))
 
-    go mempoolPoll(client)
+	go mempoolPoll(client)
 
 	// Wait until the client either shuts down gracefully (or the user
 	// terminates the process with Ctrl+C).
